c15241ed7e57d4c4975309f9203e6896276b05c1
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2025-08-05 12:53:36 +0530

    test: write tests for get all bookmarks, create a bookmark, update a bookmark by id and delete a bookmark by id

--- db/init.js
+++ db/init.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config({
-  path: process.env.NODE_ENV === "test" ? "env.test" : ".env",
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
 });
 
 const sequelize = new Sequelize({
